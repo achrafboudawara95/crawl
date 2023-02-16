35982410d7811e74b4c8ca5f2d1b6f3ec5c89211
Achraf <achrafboudawarafseg@gmail.com>
2023-02-16 01:51:47 +0100

    update crawl service

--- src/Service/CrawlService.php
+++ src/Service/CrawlService.php
@@ -2,7 +2,6 @@
 
 namespace App\Service;
 
-use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 abstract class CrawlService implements CrawlInterface
@@ -10,12 +9,9 @@ abstract class CrawlService implements CrawlInterface
     protected HttpClientInterface $httpClient;
     protected string $sourceURL;
 
-    protected Crawler $crawler;
-
-    public function __construct(HttpClientInterface $client, Crawler $crawler, string $sourceURL)
+    public function __construct(HttpClientInterface $client, string $sourceURL)
     {
         $this->httpClient = $client;
         $this->sourceURL = $sourceURL;
-        $this->crawler = $crawler;
     }
 }
