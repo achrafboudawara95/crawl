a9ee61a7cb4d43970f1a5debda555faddc77fb8e
Achraf <achrafboudawarafseg@gmail.com>
2023-02-16 10:25:59 +0100

    update functional test

---
+++ src/Entity/News.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(indexes={
+ *     @ORM\Index(name="title_idx", columns={"title"})
+ * })
+ */
+class News
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    private ?int $id = null;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image): void
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date): void
+    {
+        $this->date = $date;
+    }
+}
--- src/MessageHandler/NewsNotificationHandler.php
+++ src/MessageHandler/NewsNotificationHandler.php
@@ -2,13 +2,29 @@
 
 namespace App\MessageHandler;
 
+use App\Entity\News;
 use App\Message\NewsNotification;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class NewsNotificationHandler implements MessageHandlerInterface
 {
+    private EntityManagerInterface $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function __invoke(NewsNotification $message)
     {
+        $news = new News();
+        $news->setTitle($message->getTitle());
+        $news->setDescription($message->getDescription());
+        $news->setImage($message->getImage());
+        $news->setDate(DateTime::createFromFormat('Y-m-d\TH:i:s\Z', $message->getDate()));
 
+        $this->entityManager->persist($news);
+        $this->entityManager->flush();
     }
 }
